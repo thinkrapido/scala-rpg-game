b0dbc6cafe4b710ba1f96d7eb1a1b5acf0f0faf6
Romeo Disca <romeo.disca@gmail.com>
2017-10-12 18:02:25 +0200

    feat: add isSame and inverse methods

--- scalaCore/src/main/scala/com.jellobird.testgame/utils/world/Location.scala
+++ scalaCore/src/main/scala/com.jellobird.testgame/utils/world/Location.scala
@@ -3,7 +3,7 @@ package com.jellobird.testgame.utils.world
 /**
   * Created by Romeo Disca on 10/11/2017.
   */
-case class Location(val x: Float, val y: Float) extends Scale[Location] with Transform[Location] {
+case class Location(val x: Float, val y: Float) extends Scale[Location] with Transform[Location] with Inverse[Location] {
 
   override def scale(factor: Float) = Location(x * factor, y * factor)
 
@@ -17,5 +17,10 @@ case class Location(val x: Float, val y: Float) extends Scale[Location] with Tra
   }
 
   override def transform(location: Location, factor: Float = 1) = moveBy(location).scale(factor)
+
+  def inverse = Location(-x, -y)
+
+  def matches(location: Location, epsilon: Float = 0) =
+    (Math.abs(x - location.x) < epsilon) && (Math.abs(y - location.y) < epsilon)
 
 }
